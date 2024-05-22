53b630615cb7c53348eba8a579456e7091e9f629
Raihan <raihanm854@gmail.com>
2024-05-22 08:02:32 +0700

    admin room type

--- src/admin/admintemplate/sidebar.php
+++ src/admin/admintemplate/sidebar.php
@@ -39,7 +39,7 @@
                  </a>
              </li>
              <li>
-                 <a href="http://localhost/grancy/src/adminrooms.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
+                 <a href="http://localhost/grancy/src/admin/adminrooms.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
                      <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-nav" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                          <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                      </svg>
@@ -47,7 +47,7 @@
                  </a>
              </li>
              <li>
-                 <a href="http://localhost/grancy/src/adminroomtype.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
+                 <a href="http://localhost/grancy/src/admin/adminroomtype.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
                      <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" class="w-5 h-5 lex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-nav">
                          <path d="m8,15.5c0,1.381-1.119,2.5-2.5,2.5s-2.5-1.119-2.5-2.5,1.119-2.5,2.5-2.5,2.5,1.119,2.5,2.5ZM24,4.5v18.5c0,.552-.448,1-1,1s-1-.448-1-1v-2H2v2c0,.552-.448,1-1,1s-1-.448-1-1V1C0,.448.448,0,1,0s1,.448,1,1v6h7v-2.5c0-2.481,2.019-4.5,4.5-4.5h6c2.481,0,4.5,2.019,4.5,4.5ZM2,19h7v-2.5c0-2.481,2.019-4.5,4.5-4.5h8.5v-3H2v10Zm3.5-13c1.381,0,2.5-1.119,2.5-2.5s-1.119-2.5-2.5-2.5-2.5,1.119-2.5,2.5,1.119,2.5,2.5,2.5Z" />
                      </svg>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,15 @@ module.exports = {
         backdrop: "95%",
       },
       dropShadow: {
-        '3xl': '0 5px 3px rgba(0, 0, 2, 0.25)',
-        '4xl': [
-            '0 5px 5px rgba(0, 0, 1, 0.5)',
-        ]
-      }
+        "3xl": "0 5px 3px rgba(0, 0, 2, 0.25)",
+        "4xl": ["0 5px 5px rgba(0, 0, 1, 0.5)"],
+      },
     },
     colors: {
       nav: "#F2F0E2",
       blues: "#00113D",
       blues2: "#4BBCC7",
+      yellow: "#FFD700",
       grey: "#EBEBEB",
       black: "#000000",
       white: "#FFFFFF",
